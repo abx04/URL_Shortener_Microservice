daa29b73243eb553739203e8892c4e2707d3bd31
xxabhi04 <anandabhinav16@gmail.com>
2017-06-20 19:40:58 +0530

    persist the count variable

--- routes/url_router.js
+++ routes/url_router.js
@@ -1,32 +1,43 @@
 const express=require('express');
 const router=express.Router();
 const mongoose=require('mongoose');
+const fs=require('fs');
 
 const UrlModel=require('../models/url');
 
-let count=11000;
+let count=JSON.parse(fs.readFileSync(__dirname+'/../count.json').toString()).count;
 
 router.get('/:url',function (req,res,next) {
     let url=req.params.url;
-    count++;
     UrlModel.findOne({original_url:url},function (err,result) {
        if (err)res.send(err);
-       else if(res===null)next();
-       else res.send(result);
+       else if(!result) next();
+       else {
+           let response={
+               original_url:result.original_url,
+               shortened_url:result.shortened_url
+           };
+           res.send(response);
+       }
     });
 });
 
 router.get('/:url',function(req,res){
-    let url=req.param.url;
+    let url=req.params.url;
     let urlModel=new UrlModel({
             original_url:url,
             shortened_url:count
        });
-    urlModel.save(function (err,urlModel) {
+
+    count++;
+    fs.writeFile(__dirname+'/../count.json','{\"count\":'+count+'}')
+
+    urlModel.save(function (err,result) {
        if(err) res.send(err);
        else res.send(urlModel);
 
     });
+
 
 });
 
